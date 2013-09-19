1b54e286fdb565444b181887fb5fb2c98763dee8
Kevin Iverson <kiverson@Kevin-Iverson.local>
2013-09-19 00:29:46 -0500

    Modified for HF

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -2,17 +2,17 @@ import os
 
 DEBUG = False
 
-SITE_NAME = "Stashboard"
+SITE_NAME = "Statusboard"
 SITE_AUTHOR = "Colonel Mustache"
-SITE_URL = "http://stashbooard.appspot.com"
-REPORT_URL = "mailto:help@stashboard.org"
+SITE_URL = "http://hfstatus.appspot.com"
+REPORT_URL = "mailto:help@homefinder.com"
 
 # Twitter update settings
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 TWITTER_ACCESS_TOKEN = ''
 TWITTER_ACCESS_TOKEN_SECRET = ''
-TWITTER_HANDLE = 'stashboard_status'
+TWITTER_HANDLE = ''
 
 # RSS Feed settings
 RSS_NUM_EVENTS_TO_FETCH = 50
